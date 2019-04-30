1a0c8b5c9a3f547ba4bd5dc515cf9f7d7feb904a
samuel <samuel.tan@doorda.co.uk>
2019-04-30 10:57:25 +0100

    Initial Commit

---
+++ doorda_sdk/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["host", "util"]
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import doorda_sdk
+
+INSTALL_REQUIRES = ['pyhive[presto]==0.6.1', 'requests==2.20.0']
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='doorda_sdk',
+     version=doorda_sdk.__version__,
+     author="Samuel",
+     author_email="samuel@doorda.com",
+     description="Doorda package for access to host and API platform",
+     license="Apache License 2.0",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/doorda/doorda-python-sdk",
+     packages=["doorda_sdk"],
+     classifiers=[
+         "Programming Language :: Python :: 2",
+         "Programming Language :: Python :: 2.6",
+         "Programming Language :: Python :: 2.7",
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.3",
+         "Programming Language :: Python :: 3.4",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+         "Intended Audience :: Developers"
+     ],
+     install_requires=INSTALL_REQUIRES
+ )
